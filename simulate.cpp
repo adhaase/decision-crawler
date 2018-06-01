1d2d049ed7e9c11fb21e16d5ce1dfbc058987c3c
Alex Haase <adhaase@umich.edu>
2018-06-01 01:09:59 -0400

    updated simulate and executable files

--- simulate.cpp
+++ simulate.cpp
@@ -12,11 +12,24 @@
 
 void Simulate::promptSimulation() {
 	std::cout << "\n\n";
-	std::cout << "Okay, time to send you into the dungeon. Good luck hero!\n";
-	std::cout << "Loading ";
-	for (int i = 0; i < 5; ++i) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(750)); // SUSPENSE!
-		std::cout << ".";
+	std::cout << "Okay, time to send you into the dungeon. Are you ready to head out (y)es or (q)uit? : ";
+
+	char dungeonReady = '/0';
+	std::cin >> dungeonReady;
+	switch (dungeonReady) {
+	case 'y':
+		std::cout << "Good luck Hero! I hope to see you at the bottom floor! I'll head to the elevators now.\n";
+		for (int i = 0; i < 5; ++i) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(750)); // SUSPENSE!
+			std::cout << " ... walking ... ";
+		}
+		break;
+
+	case 'q':
+		exit(0);
+		break;
+	default:
+		std::cerr << "ERROR: Option not recognized\n";
 	}
 }
 
